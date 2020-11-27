ed31c39b9efa2f668f427581dbc1600ed3f93719
像素魔法师 <pixel-magician@outlook.com>
2020-11-27 15:44:34 +0800

    整理代码格式
    Signed-off-by: 像素魔法师 <pixel-magician@outlook.com>

--- UnityCrawler/UnityCrawler_torrent.py
+++ UnityCrawler/UnityCrawler_torrent.py
@@ -10,7 +10,7 @@ import json
 import sys
 import urllib.request
 import os
-import CrawlerFunction
+import CrawlerFunction  # 自己封装的模块
 
 
 url = 'https://unity3d.com/get-unity/download/archive'
@@ -22,7 +22,7 @@ pattern = re.compile(
     "https://download.unity3d.com/download_unity/.*?/Unity-.*?.torrent")
 temp = re.findall(pattern, strhtml.text)
 
-#对结果去重
+# 对结果去重
 result = []
 for i in temp:
     if i not in result:
@@ -33,16 +33,13 @@ path = sys.path[0]
 path1 = path + r"/Unity_torrent.json"
 path2 = path + r"/Unity_torrent_追加.json"
 
-# 读取已保存数据
 if os.path.exists(path1):
-    result_O = CrawlerFunction.ReadFromJson(path1)
-    # 获取新增的数据
-    result_Append = list(set(result).difference(set(result_O)))
-    # 保存新增的数据
-    CrawlerFunction.WriteToJson(path2,result_Append)
+    result_O = CrawlerFunction.ReadFromJson(path1)  # 读取已保存数据
+    result_Append = list(set(result).difference(set(result_O)))  # 获取新增的数据
+    CrawlerFunction.WriteToJson(path2, result_Append)  # 保存新增的数据
 
-# 保存下载的数据
-CrawlerFunction.WriteToJson(path1,result)
+
+CrawlerFunction.WriteToJson(path1, result)  # 保存下载的数据
 
 
 # 下载种子文件
